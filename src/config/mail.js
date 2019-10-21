191782154e1939f1aa06ecd8d22e5774e4f9a44c
Ruan Pablo Medeiros <pm.ruan@gmail.com>
2019-10-21 17:42:12 -0300

    api gobarber

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: process.env.MAIL_HOST,
+  port: process.env.PORT,
+  secure: false,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  },
+};
