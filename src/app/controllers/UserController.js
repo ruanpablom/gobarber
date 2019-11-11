57d02e5fab515acccc16741f4b31ad55d82f08a9
Ruan Pablo Medeiros <pm.ruan@gmail.com>
2019-11-11 18:24:48 -0300

    alterações

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,6 +1,7 @@
 import * as Yup from 'yup';
 
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
   async store(req, res) {
@@ -9,13 +10,13 @@ class UserController {
       email: Yup.string()
         .email()
         .required(),
-      passowrd: Yup.string()
+      password: Yup.string()
         .required()
         .min(6),
     });
 
     if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation fails' });
+      return res.status(400).json({ error: 'Validation fails users' });
     }
 
     const userExists = await User.findOne({ where: { email: req.body.email } });
@@ -67,13 +68,24 @@ class UserController {
     if (oldPassword && !(await user.checkPassword(oldPassword))) {
       return res.status(401).json({ error: 'Password does not match' });
     }
-    const { id, name, provider } = await user.update(req.body);
+
+    await user.update(req.body);
+
+    const { id, name, avatar } = await User.findByPk(req.userId, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     return res.json({
       id,
       name,
       email,
-      provider,
+      avatar,
     });
   }
 }
